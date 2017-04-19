a40c4482caedde831fd4026489745ad0903f3902
ezrajteboul <ezrajteboul@users.noreply.github.com>
2017-04-19 14:29:08 -0400

    edited to use 2 variables from osc message

--- analog output osc.cpp
+++ analog output osc.cpp
@@ -4,19 +4,14 @@
 |  _ \|  _| | |     / _ \  
 | |_) | |___| |___ / ___ \ 
 |____/|_____|_____/_/   \_\
-
 The platform for ultra-low latency audio and sensor processing
-
 http://bela.io
-
 A project of the Augmented Instruments Laboratory within the
 Centre for Digital Music at Queen Mary University of London.
 http://www.eecs.qmul.ac.uk/~andrewm
-
 (c) 2016 Augmented Instruments Laboratory: Andrew McPherson,
   Astrid Bin, Liam Donovan, Christian Heinrichs, Robert Jack,
   Giulio Moro, Laurel Pardue, Victor Zappi. All rights reserved.
-
 The Bela software is distributed under the GNU Lesser General Public License
 (LGPL 3.0), available here: https://www.gnu.org/licenses/lgpl-3.0.txt
 */
@@ -53,8 +48,9 @@ int parseMessage(oscpkt::Message msg){
     if (msg.match("/osc-test").popInt32(intArg).popFloat(floatArg).isOkNoMoreArgs()){
         rt_printf("received int %i and float %f\n", intArg, floatArg);
     }
-    //else if  (msg.match("/freq").popFloat(floatArg).popFloat(fA).isOkNoMoreArgs()){
-    else if  (msg.match("/freq").popFloat(floatArg).isOkNoMoreArgs()){
+    else if  (msg.match("/freq").popFloat(floatArg).popFloat(fA).isOkNoMoreArgs()){
+    	rt_printf("here\n");
+    //else if  (msg.match("/freq").popFloat(floatArg).isOkNoMoreArgs()){
     	//rt_printf("here\n");
     // we added an else if condition to make sure the received freq value is within certain range
     	if (floatArg <= 12000 && floatArg >= 40)
@@ -139,22 +135,17 @@ void cleanup(BelaContext *context, void *userData)
 
 /**
 \example analog-output/render.cpp
-
 Fading LEDs
 -----------
-
 This sketch uses a sine wave to drive the brightness of a series of LEDs 
 connected to the eight analog out pins. Again you can see the nested `for` loop 
 structure but this time for the analog output channels rather than the audio.
-
 - connect an LED in series with a 470ohm resistor between each of the analogOut pins and ground.
-
 Within the first for loop in render we cycle through each frame in the analog 
 output matrix. At each frame we then cycle through the analog output channels 
 with another for loop and set the output voltage according to the phase of a 
 sine tone that acts as an LFO. The analog output pins can provide a voltage of 
 ~4.092V.
-
 The output on each pin is set with `analogWrite()` within the for loop that 
 cycles through the analog output channels. This needs to be provided with 
 arguments as follows `analogWrite(context, n, channel, out)`. Channel is 
@@ -162,7 +153,6 @@ where the you give the address of the analog output pin (in this case we cycle
 through each pin address in the for loop), out is the variable that holds the 
 desired output (in this case set by the sine wave) and `n` is the frame number 
 (given by the other for loop).
-
 Notice that the phase of the brightness cycle for each led is different. This 
 is achieved by updating a variable that stores a relative phase value. This 
 variable is advanced by pi/4 (1/8 of a full rotation) for each channel giving 
